c417be0f457b7197a37c2c9aaf35598fd7d0e465
Ceyda <ceydaonemli2@gmail.com>
2022-09-16 22:30:11 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./app.css";
+import FormInput from "./components/FormInput";
+const App = () => {
+  return (
+    <div className="app">
+      <form>
+        <FormInput placeholder="Username"/>
+        <FormInput placeholder="Email"/>
+        <FormInput placeholder="Full Name"/>
+        <FormInput placeholder="Sth else"/>
+      </form>
+    </div>
+  );
+};
+export default App;
